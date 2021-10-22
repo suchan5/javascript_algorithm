dc692a54562cd3934d7ed86f5a2d90420b887d18
suchan5 <suchan5@hotmail.com>
2021-10-22 11:09:13 +0800

    등수구하기 문제

--- js100_solutions/010.js
+++ js100_solutions/010.js
@@ -168,6 +168,12 @@
 //     console.log(' '.repeat(8-i-i) + '*'.repeat(i*2+1));
 // }
 
+// let n=5;
+// for (let i=1; i<=9; i+=2) {
+//     console.log(' '.repeat(9-i) + '*'.repeat(i));
+// }
+
+
 
 
 
