528a4dc0f14a895388631ed72f7d7d1021cb32f2
suchan5 <suchan5@hotmail.com>
2022-09-14 13:43:00 +0800

    solutions updated

--- boj_solutions/01_arithmetic_operation/07_10869.js
+++ boj_solutions/01_arithmetic_operation/07_10869.js
@@ -13,12 +13,12 @@ solution(input[0], input[1]);
 
 
 // readFile없이 이렇게 하면 더 간단. 
-function solution(A, B) {
-    console.log(A+B);
-    console.log(A-B);
-    console.log(A*B);
-    console.log(parseInt(A/B)); // Math.floor(A/B);으로 해도 된다
-    console.log(A%B);
-}
-const input = [7, 3];
-solution(input[0], input[1]);
+// function solution(A, B) {
+//     console.log(A+B);
+//     console.log(A-B);
+//     console.log(A*B);
+//     console.log(parseInt(A/B)); // Math.floor(A/B);으로 해도 된다
+//     console.log(A%B);
+// }
+// const input = [7, 3];
+// solution(input[0], input[1]);
